23d31533989a49124037d38703dbc397fccaa11d
“Marclean” <marclean0965@gmail.com>
2026-07-18 15:14:24 +0800

    Done with laboratory 3

---
+++ L03_getting-started-react/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+    <h1>ABOUT ME</h1>
+    <h3 >Forteza, Marclean L.</h3>
+    <h4>BS INFORMATION SYSTEMS 3</h4>
+    <p>I use calculator even the math question is 1x1 haha.</p>
+    <p>As i enroll on this course and attending in first day, i felt so nervous knowing that our instructor will be Sir Elmer. But as time passed by i realize that if im willing to learn some skills i need to work for it. on our first day we tackle about the importance and the difference between web dev and app dev. Also, i learned how to set up ssh key that made my brain so damage haha, but after those challenges i manage to learn to set it up. Today, I've Learned the function of react on DOM and also i learned how to set up vite.</p>
+</>
+  )
+}
+
+export default App
